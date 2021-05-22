528affe79fdd0742c2e6d8f80780f1d46ac632ad
Jobayer-Majumder <jobayermojumder70@gmail.com>
2021-05-22 18:14:54 +0600

    fixed deploy problem

--- src/Components/FilteringCategories/FilteringCategory/FilteringCategory.js
+++ src/Components/FilteringCategories/FilteringCategory/FilteringCategory.js
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
 import FilterHeading from '../FilterHeading/FilterHeading';
-import { createClient } from 'pexels';
+// import { createClient } from 'pexels';
 import { createApi } from 'unsplash-js';
 
 
 
-const api = '563492ad6f91700001000001f1232539a06a4f7f9e494e495e578da2'
+// const api = '563492ad6f91700001000001f1232539a06a4f7f9e494e495e578da2'
 const accessKey = 'El-MFtITW42ZMuDxC-xjqdliucBmCZNZ_twRXqIsvd0';
-const secretKey = 'aRkF8JSXCnAr_s67Zf33-a5SeXJee_xCzwLTuIZQVcI';
+// const secretKey = 'aRkF8JSXCnAr_s67Zf33-a5SeXJee_xCzwLTuIZQVcI';
 const unsplash = createApi({ accessKey: `${accessKey}` });
 
 
 const FilteringCategory = () => {
-    const [ photos, setPhotos] = useState([]);
+    // const [ photos, setPhotos] = useState([]);
 
     unsplash.photos.get({ per_page: 10 }).then(result => {
         if (result.errors) {
